1d8af5ac04e36f089008f88f0bbf79f8fb943e2d
Katie <katherynmknutson@lewisu.edu>
2021-12-01 10:23:59 -0600

    pics + descriptions

--- pictureDataReader.java
+++ pictureDataReader.java
@@ -7,6 +7,7 @@
  * You call it near the beginning of your program to create the PictureDataReader objects 
  * that will serve as the model for your application
  */
+
 public class pictureDataReader {
     
 }
