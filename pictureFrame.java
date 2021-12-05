e6dc4d4f65b2f51a470b178a1d5d4270790e43c0
Katie <katherynmknutson@lewisu.edu>
2021-12-05 16:51:46 -0600

    yeet

--- pictureFrame.java
+++ pictureFrame.java
@@ -14,7 +14,39 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class pictureFrame extends JFrame{
+    //code from klumps in class menu and mouse frame 
+    public void setupMainMenu() {   
+        JMenuBar mbar = new JMenuBar();
+        JMenu mnuFile = new JMenu("File");
+        JMenu mnuHelp = new JMenu("Help");
+        mbar.add(mnuFile);
+        mbar.add(mnuHelp);
+
+        JMenuItem miSave = new JMenuItem("Save");
+        mnuFile.add(miSave);
+        JMenuItem miExit = new JMenuItem("Exit");
+        miExit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        mnuFile.add(miExit);
+        JMenuItem miAbout = new JMenuItem("About");
+        miAbout.addActionListener(
+            new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(null,"Katie Knutson");
+                }
+            }
+        );
+        mnuHelp.add(miAbout);
+        setJMenuBar(mbar);
+    }
+
+    
     public pictureFrame(){
+        //code from notes 
+        setupMainMenu();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Picture Frame");
         setBounds(100,100,500,500);
@@ -24,8 +56,13 @@ public class pictureFrame extends JFrame{
         panCenter.setBackground(Color.RED);
         JPanel panSouth = new JPanel();
         panSouth.setBackground(Color.WHITE);
+        //add all of the buttons to the botton 
         JButton prev = new JButton("Prev");
         panSouth.add(prev);
+        JButton save = new JButton ("Save");
+        panSouth.add(save);
+        JButton next = new JButton ("Next");
+        panSouth.add(next);
         c.add(panCenter, BorderLayout.CENTER);
         c.add(panSouth, BorderLayout.SOUTH);
 
